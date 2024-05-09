9f75b7e2bbd30c1f40032b3b1b86bbd6f3da0b15
Archanial <faceaccont@gmail.com>
2024-05-09 14:30:07 +0200

    Validates metadata name

--- spa_src/SPA.PQL/QueryElements/PQLWithCondition.cs
+++ spa_src/SPA.PQL/QueryElements/PQLWithCondition.cs
@@ -6,31 +6,56 @@ namespace SPA.PQL.QueryElements {
     internal sealed class PQLWithCondition : PQLBaseCondition {
         public required PQLWithConditionReference LeftReference { get; set; }
         public required PQLWithConditionReference RightReference { get; set; }
+
+        private static readonly string[] ValidMetadataNames = [ 
+            "procName", "varName", "value", "stmt#"
+        ];
         
         public override void Validate(PQLQuery query, PQLQueryValidationResult result)
         {
-            if (LeftReference.Type is PQLWithConditionReferenceType.Variable or PQLWithConditionReferenceType.Metadata)
-            {
-                if (query.Variables.Any(x => x.Name == LeftReference.VariableName))
-                {
-                    result.Errors.Add($"Variable not declared: {LeftReference.VariableName}");
-                }
-            }
-            
-            if (RightReference.Type is PQLWithConditionReferenceType.Variable or PQLWithConditionReferenceType.Metadata)
-            {
-                if (query.Variables.Any(x => x.Name == RightReference.VariableName))
-                {
-                    result.Errors.Add($"Variable not declared: {RightReference.VariableName}");
-                }
-            }
-            
-            //TODO: Validate the Metadata name
+            ValidateReference(LeftReference, query, result);
+            ValidateReference(RightReference, query, result);
         }
 
         public override void Evaluate(IPKBInterface pkbApi, List<EvaluatedVariable> variables)
         {
             throw new NotImplementedException();
         }
+        
+        private static void ValidateReference(PQLWithConditionReference reference, 
+            PQLQuery query,
+            PQLQueryValidationResult result)
+        {
+            switch (reference.Type)
+            {
+                case PQLWithConditionReferenceType.Variable:
+                    CheckVariableDeclaration(query, result, reference.VariableName);
+                    break;
+                case PQLWithConditionReferenceType.Metadata:
+                    CheckVariableDeclaration(query, result, reference.VariableName);
+                    CheckMetadataName(result, reference.VariableName);
+                    break;
+            }
+        }
+
+        private static void CheckVariableDeclaration(PQLQuery query,
+            PQLQueryValidationResult result,
+            string? variableName)
+        {
+            if(!IsVariableDeclared(query, variableName))
+                result.Errors.Add($"Variable not declared: {variableName ?? "null"}");
+        }
+        
+        private static void CheckMetadataName(PQLQueryValidationResult result, string? metadataName)
+        {
+            if(!IsValidMetadataName(metadataName))
+                result.Errors.Add($"Invalid metadata name {metadataName ?? "null"}");
+        }
+        
+        private static bool IsVariableDeclared(PQLQuery query, string? variableName) 
+            => !string.IsNullOrEmpty(variableName) && query.Variables.Any(x => x.Name == variableName);
+        
+        private static bool IsValidMetadataName(string? name) 
+            => !string.IsNullOrEmpty(name) && ValidMetadataNames.Contains(name);
     }
 }
