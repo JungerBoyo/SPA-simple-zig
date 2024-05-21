b4270204b115516fcae146a30acac75fafa82625
Piotr Szuflicki <piotr.szuflicki@goelett.com>
2024-05-21 10:39:21 +0200

    Added another test

--- spa_src/SPA.PQL.Tests/EvaluatorIntegrationTests.cs
+++ spa_src/SPA.PQL.Tests/EvaluatorIntegrationTests.cs
@@ -4,13 +4,31 @@ using Xunit;
 
 namespace SPA.PQL.Tests {
     public sealed class EvaluatorIntegrationTests {
-
         [Fact]
         public void Test1()
         {
             var path = Path.Combine(Environment.CurrentDirectory, "TestCodes/Test1.simple");
 
             var simpleQuery = "assign x; Select x";
+
+            using (var evaluator = new PQLEvaluator(simpleQuery, new PKBInterface()))
+            {
+                var validationResult = evaluator.ValidateQuery();
+
+                Assert.Empty(validationResult.Errors);
+                
+                var result = evaluator.Evaluate(path);
+                Assert.IsType<VariableQueryResult>(result);
+                var casted = result as VariableQueryResult;
+            }
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "TestCodes/Test1.simple");
+
+            var simpleQuery = "procedure x; Select x";
 
             using (var evaluator = new PQLEvaluator(simpleQuery, new PKBInterface()))
             {
