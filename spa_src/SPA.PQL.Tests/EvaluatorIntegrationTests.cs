e2ef9067727a8b885fa2a5c0c7cac2aca23b6fa6
Kacper Świderek <80694@student.pb.edu.pl>
2024-06-06 17:25:46 +0200

    align test to changes

--- spa_src/SPA.PQL.Tests/EvaluatorIntegrationTests.cs
+++ spa_src/SPA.PQL.Tests/EvaluatorIntegrationTests.cs
@@ -88,13 +88,13 @@ namespace SPA.PQL.Tests {
 
             var simpleQuery = "assign a, b, c;Select a such that Follows(b, a) and Follows (c, b)";
 
-            using (var evaluator = new PQLEvaluator(simpleQuery, new PKBInterface()))
+            using (var evaluator = new PQLEvaluator(new PKBInterface(), path))
             {
-                var validationResult = evaluator.ValidateQuery();
+                var validationResult = evaluator.ValidateQuery(simpleQuery);
 
                 Assert.Empty(validationResult.Errors);
                 
-                var result = evaluator.Evaluate(path);
+                var result = evaluator.Evaluate();
                 Assert.IsType<VariableQueryResult>(result);
                 
                 var variableResult = (result as VariableQueryResult)!;
